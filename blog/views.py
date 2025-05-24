3069f0163a0ec3c0dbef6fab22240f7dcd48234d
todd427 <todd@toddwriter.com>
2025-05-24 14:03:43 -0700

    Sendmail WORKS! (with HACK)

--- blog/views.py
+++ blog/views.py
@@ -37,4 +37,33 @@ def post_detail(request, year, month, day, post):
             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
 
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    form = None
+    if (request.method == 'POST'):
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if (form.is_valid()):
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read {post.title}"
+            message = f"Read \"{post.title}\" at {post_url}\n\n{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, from_email=None, recipient_list=[cd['to']])
+            sent = True
+        else:
+            form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+         {'post': post, 'form': form, 'sent': sent}
+         )
+            
+    
+    
 # Create your views here.
