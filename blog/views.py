8d9aafca836861272088c82c4c6dfa1c54587799
todd427 <todd@toddwriter.com>
2025-05-28 10:49:17 -0700

    added search

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,23 @@ from .models import Post
 from django.views.generic import ListView
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
+from .forms import SearchForm
 
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+                ).filter(search=query)
+    return render(request, 'blog/post/search.html', 
+                  {'form': form, 'query': query, 'results': results})
 
 class PostListView(ListView):
     """
@@ -57,12 +73,14 @@ def post_detail(request, year, month, day, post):
     similar_posts = similar_posts.annotate(
         same_tags=Count('tags')
         ).order_by('-same_tags', '-publish')[:4]
-    return render(request, 'blog/post/detail.html', {
+    return render(request, 'blog/post/detail.html', 
+        {
         'post': post, 
         'comments': comments, 
         'form': form, 
-        'similar_posts': similar_posts}
-        )
+        'similar_posts': similar_posts
+        }
+    )
 
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
